26fec6624f466da64ac7d4610cd21a8a0e0d9d39
Miguel-Basilio <lokoangel06@gmail.com>
2021-09-04 20:51:35 -0700

    Initial Commit

---
+++ input_Integer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  input_Integer
+//
+//  Created by AB on 9/4/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
