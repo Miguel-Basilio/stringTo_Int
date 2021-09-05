4af324bd92bd9b10ff6c398e0c6cc2da8d3a766d
Miguel-Basilio <lokoangel06@gmail.com>
2021-09-04 22:57:19 -0700

    first commit

--- input_Integer/main.cpp
+++ input_Integer/main.cpp
@@ -6,9 +6,60 @@
 //
 
 #include <iostream>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+/*
+ 
+example 1
+int convert(string str){
+    int num =0, size = str.size();
+    for (int i = 0, j = size; i < size; i++)
+        num+=(str[--j]-'0') * pow(10,i);
+    return num;
+}
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    string strOfDigits;
+    cout << "Enter a Whole Number: " << endl;
+    getline (cin, strOfDigits);
+    int aNumber= convert(strOfDigits);
+    cout << "Converted String to Int: " << aNumber << endl;
+ 
+ retun 0;
+ }
+ */
+
+//example 2
+// function for converting string to integer
+int stringTointeger(string str)
+{
+    int aNumber = 0;
+    for (int i = 0; i < str.length(); i++) {
+  
+        // Since ASCII value of character from '0'
+        // to '9' are contiguous. So if we subtract
+        // '0' from ASCII value of a digit, we get
+        // the integer value of the digit.
+        aNumber = aNumber * 10 + (str[i] - '0');
+    }
+    return aNumber;
+}
+
+
+
+
+int main(int argc, const char * argv[]) {
+    string strOfDigits ;
+    cout << "Enter Whole Number: " << endl;
+    cin >> strOfDigits;
+        int aNumber = stringTointeger(strOfDigits);
+        cout << "Converted String to Number is: " <<aNumber << endl;
+ 
+ return 0;
+ 
 }
